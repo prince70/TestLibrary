1a46c105b3d8520fd86e73c8c60f075435f7e972
prince70 <942892084@qq.com>
2019-07-24 14:06:59 +0800

    修改build.gradle

---
+++ nwjlibrary/src/main/java/com/niwj/nwjlibrary/Test.java
@@ -0,0 +1,10 @@
+package com.niwj.nwjlibrary;
+
+/**
+ * Created by Administrator on 2019/7/24.
+ */
+public class Test {
+    public double addition(double addnum1, double addnum2) {
+        return addnum1 + addnum2;
+    }
+}
